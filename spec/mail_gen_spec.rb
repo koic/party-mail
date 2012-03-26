d9f6f2605207d1e0760cd53a43789a501f7c149b
ITO Koichi <koic.ito@gmail.com>
2012-03-26 14:51:02 +0900

    でり坊 五反田店が閉店した

--- spec/mail_gen_spec.rb
+++ spec/mail_gen_spec.rb
@@ -5,16 +5,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/mail_gen')
 describe 'メール文の生成' do
   before(:each) do
     @text = <<TEXT
-美味しい料理と日本酒の飲み会のご案内です。
+ボヤキ飲み会のご案内です。
 
  * 日時: 02月19日(金) 19:30 乾杯
- * 場所: でり坊 五反田店 (http://www.delibo.com/gotanda/)
- * 予算: 4,000円
+ * 場所: 立ち呑み処 ボヤキ (http://r.tabelog.com/tokyo/A1311/A131101/13105840/)
+ * 予算: 2,500円
  * 予約者名: koic
- * 緊急連絡先 (koic携帯) : 090-9999-9999 / nomarebu@nomare.bu
- * 参加者: igaiga555, beakmark, chiba777
+ * 緊急連絡先 (koic携帯) : 090-9999-9999 / nomarebu@example.com
+ * 参加者: chibamem, ryhmrt, kunitoo
 
-当日は、19:20に五反田駅の改札前に集合をお願いします。
+当日は、19:20に御徒町駅南口の改札前に集合をお願いします。
 
   予約の都合上、万が一、キャンセル、または遅刻される場合は、
   当日16:00までに必ずご連絡ください。
