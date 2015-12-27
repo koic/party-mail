f947625d58869cd60dfa6a5539ee65b85b13be91
Koichi ITO <koic.ito@gmail.com>
2015-12-27 22:47:43 +0900

    Drop support for Ruby 1.9 and below

--- lib/mail_gen.rb
+++ lib/mail_gen.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 require 'rubygems'
 require 'haml'
 require 'yaml'
--- spec/mail_gen_spec.rb
+++ spec/mail_gen_spec.rb
@@ -1,5 +1,3 @@
-# coding: utf-8
-
 require File.expand_path(File.dirname(__FILE__) + '/../lib/mail_gen')
 
 describe 'generate_text' do
